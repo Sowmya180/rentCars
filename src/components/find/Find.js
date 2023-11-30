ab91d29b37212d6ea0e0045848a85402d9ff79e3
KSowmya18031 <krishnamoorthi.sowmya@divum.in>
2023-11-30 10:28:29 +0530

    stylings

--- src/components/find/Find.js
+++ src/components/find/Find.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { A11y, Autoplay, Navigation, Pagination, Scrollbar } from "swiper";
+import { A11y, Navigation, Pagination, Scrollbar } from "swiper";
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Card from "./Card";
@@ -14,8 +14,140 @@ const Find = () => {
   const [showForm, setShowForm] = useState(false);
   const [selectedCar, setSelectedCar] = useState(null);
 
+  const cars = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1599912027806-cfec9f5944b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGF1ZGl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      make: "Audi",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "823$",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1555215695-3004980ad54e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Ym13fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      make: "BMW",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "213$",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2hldnJvbGV0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      make: "Chevrolet",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "267",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1609521263047-f8f205293f24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmlzc2FufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      make: "Nisaan",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "254",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1616788494707-ec28f08d05a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVyY2VkZXMlMjBiZW56fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      make: "Mercedes-Benz",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "132",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1619682817481-e994891cd1f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHRveW90YXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+      make: "Toyota",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "212",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1588636142475-a62d56692870?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8amVlcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+      make: "Jeep",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "231",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      make: "Ford",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "123$",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1606016159991-dfe4f2746ad5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dGVzbGF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      make: "Tesla",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "231",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1580274455191-1c62238fa333?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9yc2NoZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+      make: "Porsche",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "291",
+    },
+    {
+      image:
+        " https://images.unsplash.com/photo-1599912027611-484b9fc447af?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      make: "Rolls Royce",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "531",
+    },
+    {
+      image:
+        "https://images.pexels.com/photos/6894429/pexels-photo-6894429.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      make: "Maaserati",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "201",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGx1eHVyeSUyMGNhcnN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      make: "Range Rover",
+      capacity: 5,
+      bagCapacity: 5,
+      mileage: "Unlimited mileage",
+      transmission: "automatic",
+      price: "271",
+    },
+  ];
+
   const handleClick = (car) => {
-    console.log("handleClick triggered");
     setSelectedCar(car);
     setShowForm(true);
   };
@@ -51,266 +183,11 @@ const Find = () => {
           onSwiper={(swiper) => console.log(swiper)}
           onSlideChange={() => console.log("slide change")}
         >
-          <SwiperSlide
-            id="carsSection"
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1599912027806-cfec9f5944b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGF1ZGl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-                make: "Audi",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "823$",
-              })
-            }
-          >
-            <Card
-              image="https://images.unsplash.com/photo-1599912027806-cfec9f5944b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGF1ZGl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-              make="Audi"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1555215695-3004980ad54e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Ym13fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-                make: "BMW",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "213$",
-              })
-            }
-          >
-            <Card
-              image="https://images.unsplash.com/photo-1555215695-3004980ad54e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Ym13fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              make="BMW"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2hldnJvbGV0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-                make: "Chevrolet",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "267",
-              })
-            }
-          >
-            <Card
-              image="https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2hldnJvbGV0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              make="Chevrolet"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1609521263047-f8f205293f24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmlzc2FufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-                make: "Nisaan",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "254",
-              })
-            }
-          >
-            <Card
-              image="https://images.unsplash.com/photo-1609521263047-f8f205293f24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmlzc2FufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              make="Nissan"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1616788494707-ec28f08d05a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVyY2VkZXMlMjBiZW56fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-                make: "Mercedes-Benz",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "132",
-              })
-            }
-          >
-            <Card
-              image="https://images.unsplash.com/photo-1616788494707-ec28f08d05a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVyY2VkZXMlMjBiZW56fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              make="MercedesBenz"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1619682817481-e994891cd1f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHRveW90YXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-                make: "Toyota",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "212",
-              })
-            }
-          >
-            <Card
-              image="https://images.unsplash.com/photo-1619682817481-e994891cd1f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHRveW90YXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
-              make="Toyota"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1588636142475-a62d56692870?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8amVlcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-                make: "Jeep",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "231",
-              })
-            }
-          >
-            <Card
-              image="https://images.unsplash.com/photo-1588636142475-a62d56692870?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8amVlcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
-              make="Jeep"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1604108415419-6d4bd73a1c2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZvcmR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-                make: "Ford",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "123$",
-              })
-            }
-          >
-            <Card
-              image="https://images.unsplash.com/photo-1604108415419-6d4bd73a1c2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZvcmR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-              make="Ford"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1606016159991-dfe4f2746ad5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dGVzbGF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-                make: "Tesla",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "231",
-              })
-            }
-          >
-            <Card
-              image="https://images.unsplash.com/photo-1606016159991-dfe4f2746ad5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dGVzbGF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-              make="Tesla"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1580274455191-1c62238fa333?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9yc2NoZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-                make: "Porsche",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "291",
-              })
-            }
-          >
-            <Card
-              image="https://images.unsplash.com/photo-1580274455191-1c62238fa333?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9yc2NoZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
-              make="Porsche"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1599912027806-cfec9f5944b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGF1ZGl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-                make: "Rolls Royce",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "531",
-              })
-            }
-          >
-            <Card
-              image="https://images.pexels.com/photos/3764984/pexels-photo-3764984.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              make="Rolls Royce"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.pexels.com/photos/6894429/pexels-photo-6894429.jpeg?auto=compress&cs=tinysrgb&w=1600",
-                make: "Maaserati",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "201",
-              })
-            }
-          >
-            <Card
-              image="https://images.pexels.com/photos/6894429/pexels-photo-6894429.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              make="Maaserati"
-            />
-          </SwiperSlide>
-
-          <SwiperSlide
-            onClick={() =>
-              handleClick({
-                image:
-                  "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGx1eHVyeSUyMGNhcnN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-                make: "Range Rover",
-                capacity: 5,
-                bagCapacity: 5,
-                mileage: "Unlimited mileage",
-                transmission: "automatic",
-                price: "271",
-              })
-            }
-          >
-            <Card
-              image="https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGx1eHVyeSUyMGNhcnN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              make="Range Rover"
-            />
-          </SwiperSlide>
+          {cars.map((car, index) => (
+            <SwiperSlide key={index} onClick={() => handleClick(car)}>
+              <Card image={car.image} make={car.make} />
+            </SwiperSlide>
+          ))}
         </Swiper>
         {showForm && selectedCar && (
           <LocationForm
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Hero.module.css";
-import { AiOutlineSearch } from "react-icons/ai";
+
 const Hero = () => {
-  return <div className={styles.Hero}></div>;
+  const carPhotos = [
+    "https://images.unsplash.com/photo-1519641471654-76ce0107ad1b?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    "https://images.unsplash.com/photo-1592929881470-65c6db486987?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1568559598349-dbf322d50a48?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1603584173870-7f23fdae1b7a?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1587376848075-0e59aed09b98?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1604705528495-8c3d81031dae?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    ];
+
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+
+  useEffect(() => {
+    const updatePhotoIndex = () => {
+      setCurrentPhotoIndex((prevIndex) =>
+        prevIndex === carPhotos.length - 1 ? 0 : prevIndex + 1
+      );
+    };
+
+    const intervalId = setInterval(updatePhotoIndex, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className={styles.Hero}>
+      {/* Display the current car photo with the same styles */}
+      <img
+        className={styles.HeroImage}
+        src={carPhotos[currentPhotoIndex]}
+        alt={`Car ${currentPhotoIndex + 1}`}
+        srcSet={`${carPhotos[currentPhotoIndex]} 1x, ${carPhotos[currentPhotoIndex]} 2x`}
+      />
+    </div>
+  );
 };
 
 export default Hero;
